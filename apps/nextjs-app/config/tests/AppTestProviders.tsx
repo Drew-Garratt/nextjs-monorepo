475704ef44990818d44c4730030f38b0230cb616
Drew Garratt <drew_garratt@mckinsey.com>
2024-01-23 12:30:51 +0000

    fix: remove unsed provider

--- apps/nextjs-app/config/tests/AppTestProviders.tsx
+++ apps/nextjs-app/config/tests/AppTestProviders.tsx
@@ -1,11 +1,6 @@
 import type { FC, PropsWithChildren } from 'react';
 import { AppProviders } from '../../src/providers/app-providers';
-import { I18nextTestStubProvider } from './I18nextTestStubProvider';
 
 export const AppTestProviders: FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <AppProviders lang="en">
-      <I18nextTestStubProvider>{children}</I18nextTestStubProvider>
-    </AppProviders>
-  );
+  return <AppProviders lang="en">{children}</AppProviders>;
 };
